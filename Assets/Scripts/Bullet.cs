7768fd47ab7c57bc5937c8477dbe0fe19953459e
ne241152 <ne241152@senshu-u.jp>
2026-07-01 12:48:21 +0900

    構造

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+//テスト
 
 public class Bullet : MonoBehaviour
 {
-    public int damage = 1;
-
     void Start() { Destroy(gameObject, 3.0f); } // 画面外に飛んでいった弾を3秒後に自動消滅
 
     void OnTriggerEnter2D(Collider2D col)
     {
         // 当たった相手がEnemyタグを持っていたら
         if (col.CompareTag("Enemy")) {
-            col.GetComponent<Enemy>().TakeDamage(damage); // 敵にdamageを与える
+            col.GetComponent<Enemy>().TakeDamage(1); // 敵に1ダメージを与える
             Destroy(gameObject); // 弾自身は消える
         }
     }
